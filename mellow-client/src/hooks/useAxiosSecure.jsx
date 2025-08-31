ea9e8e3b58738ce7866c72562fab9edaa615f7b7
MD. Bashir Al Lazim <md.bashirallazim@gmail.com>
2025-08-31 21:55:33 +0600

    client_tanstack query installed and useAxiosSecure created

---
+++ mellow-client/src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { signOut } from "firebase/auth";
+import toast from "react-hot-toast";
+import auth from "../firebase/firebase.config";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const axiosSecure = axios.create({
+    baseURL: '',    //add server url
+})
+
+const useAxiosSecure = () => {
+
+    const navigate = useNavigate()
+
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('accessToken')
+        // console.log('request stopped by interceptors', token)
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    });
+
+    useEffect(() => {
+        axiosSecure.interceptors.response.use(res => {
+            return res;
+        }, (error) => {
+            if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+                toast.error('Unauthorized! Forcing logout...')
+                signOut(auth)
+                navigate('/login');
+            }
+        })
+    }, [])
+
+    return axiosSecure;
+};
+
+export default useAxiosSecure;
